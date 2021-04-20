a55be749387b04dc671f94c0bf8e750d7e7eb0a0
develohome <77947418+develohome@users.noreply.github.com>
2021-04-20 14:45:20 -0300

    pizzaria

---
+++ script.js
@@ -0,0 +1,256 @@
+var menuPizza = [
+    {
+        nome: 'Alho',
+        preco: 24.99,
+        ingreditente: 'mussarela coberta c/ alho',
+        src: './img/menuPizza/p1.jpg'
+    },
+    {
+        nome: 'Atum',
+        preco: 24.99,
+        ingreditente: 'Atum coberto c/ cebola',
+        src: './img/menuPizza/p2.jpg'
+    },
+    {
+        nome: 'Bacon',
+        preco: 24.99,
+        ingreditente: 'Bacon e mussarela',
+        src: './img/menuPizza/p3.jpg'
+    },
+    {
+        nome: 'Caipira',
+        preco: 24.99,
+        ingreditente: 'Frango e milho cobertos c/ catupiry',
+        src: './img/menuPizza/p4.jpg'
+    },
+    {
+        nome: 'Calabresa',
+        preco: 24.99,
+        ingreditente: 'Calabresa coberta c/ cebola',
+        src: './img/menuPizza/p14.jpg'
+    },
+    {
+        nome: 'Calabresa c/ catupiry',
+        preco: 24.99,
+        ingreditente: 'Calabresa coberta c/ catupiry',
+        src: './img/menuPizza/p5.jpg'
+    },
+    {
+        nome: 'Catupiry',
+        preco: 24.99,
+        ingreditente: 'catupiry',
+        src: './img/menuPizza/p6.jpg'
+    },
+    {
+        nome: 'Escarola',
+        preco: 24.99,
+        ingreditente: 'escarola coberta c/ mussarela',
+        src: './img/menuPizza/p7.jpg'
+    },
+    {
+        nome: 'Frango c/ catupiry',
+        preco: 24.99,
+        ingreditente: 'frango coberto c/ catypiry',
+        src: './img/menuPizza/p8.jpg'
+    },
+    {
+        nome: 'Milho',
+        preco: 24.99,
+        ingreditente: 'milho coberto c/ mussarela',
+        src: './img/menuPizza/p9.jpg'
+    },
+    {
+        nome: 'Mista',
+        preco: 24.99,
+        ingreditente: 'presunto coberto c/ mussarela ',
+        src: './img/menuPizza/p10.jpg'
+    },
+    {
+        nome: 'Mussarela',
+        preco: 24.99,
+        ingreditente: 'mussarela',
+        src: './img/menuPizza/p11.jpg'
+    },
+    {
+        nome: 'Portuguesa',
+        preco: 24.99,
+        ingreditente: 'presunto, ovo, ervilha e milho coberto c/ mussarela',
+        src: './img/menuPizza/p12.jpg'
+    },
+    {
+        nome: 'Toscana',
+        preco: 24.99,
+        ingreditente: 'calabresa coberta c/ mussarela',
+        src: './img/menuPizza/p13.jpg'
+    },
+    {
+        nome: '2 Queijos',
+        preco: 24.99,
+        ingreditente: 'catupiry coberto c/ mussarela',
+        src: './img/menuPizza/p15.jpg'
+    },
+    {
+        nome: 'Abobrinha',
+        preco: 24.99,
+        ingreditente: 'abobrinha mussarela ou parmesão e bacon',
+        src: './img/menuPizza/p16.jpg'
+    },
+    {
+        nome: 'Berinjela',
+        preco: 24.99,
+        ingreditente: 'berinjela mussarela ou parmesão e bacon',
+        src: './img/menuPizza/p17.jpg'
+    },
+    {
+        nome: 'À moda',
+        preco: 27.99,
+        ingreditente: 'calabresa mussarela e bacon',
+        src: './img/menuPizza/p18.jpg'
+    },
+    {
+        nome: 'Brócolis',
+        preco: 27.99,
+        ingreditente: 'brócolis, bacon, alho frito coberto c/ mussarela',
+        src: './img/menuPizza/p19.jpg'
+    },
+    {
+        nome: 'Danados',
+        preco: 27.99,
+        ingreditente: 'frango cheddar, bacon e batata palha',
+        src: './img/menuPizza/p20.jpg'
+    },
+    {
+        nome: 'Italiana',
+        preco: 27.99,
+        ingreditente: 'lombo, bacon, milho e mussarela',
+        src: './img/menuPizza/p21.jpg'
+    },
+    {
+        nome: 'Lombo',
+        preco: 27.99,
+        ingreditente: 'lombo, catupiry e cebola',
+        src: './img/menuPizza/p22.jpg'
+    },
+    {
+        nome: 'Marguerita',
+        preco: 27.99,
+        ingreditente: 'mussarela, tomate, parmesão e manjericão',
+        src: './img/menuPizza/p23.jpg'
+    },
+    {
+        nome: 'Napolitana',
+        preco: 27.99,
+        ingreditente: 'mussarela tomate e parmesão',
+        src: './img/menuPizza/p24.jpg'
+    },
+    {
+        nome: 'Pepperoni',
+        preco: 27.99,
+        ingreditente: 'mussarela e pepperoni',
+        src: './img/menuPizza/p25.jpg'
+    },
+    {
+        nome: 'Rúcula',
+        preco: 27.99,
+        ingreditente: 'mussarela de búfula rúcula e tomate',
+        src: './img/menuPizza/p26.jpg'
+    },
+    {
+        nome: 'Veneza',
+        preco: 27.99,
+        ingreditente: 'Atum, palmito e catupiry',
+        src: './img/menuPizza/p27.jpg'
+    },
+    {
+        nome: '4 Queijos',
+        preco: 27.99,
+        ingreditente: 'Provolone, mussarela, parmesão e catupiry',
+        src: './img/menuPizza/p28.jpg'
+    },
+    {
+        nome: 'Carne Seca',
+        preco: 29.99,
+        ingreditente: 'carne seca, catupiry e cebola',
+        src: './img/menuPizza/p29.jpg'
+    },
+    {
+        nome: 'Peito de peru',
+        preco: 29.99,
+        ingreditente: 'peito de peru e catupiry',
+        src: './img/menuPizza/p30.jpg'
+    },
+    {
+        nome: 'Bacalhau',
+        preco: 39.99,
+        ingreditente: 'bacalhau catupiry e cebola',
+        src: './img/menuPizza/p31.jpg'
+    },
+    {
+        nome: 'Camarão',
+        preco: 44.99,
+        ingreditente: 'camarão e catupiry',
+        src: './img/menuPizza/p32.jpg'
+    },
+    {
+        nome: 'Especial 1',
+        preco: 44.99,
+        ingreditente: 'camarão e catupiry',
+        src: './img/menuPizza/p33.jpg'
+    },
+    {
+        nome: 'Especial 2',
+        preco: 44.99,
+        ingreditente: 'camarão e catupiry',
+        src: './img/menuPizza/p34.jpg'
+    },
+    {
+        nome: 'Especial 3',
+        preco: 44.99,
+        ingreditente: 'camarão e catupiry',
+        src: './img/menuPizza/p35.jpg'
+    },
+];
+const class_menuPizza = document.querySelector('.menuPizza');
+
+for(let i = 0 ; i < menuPizza.length ; i++){
+    let criar_card = document.createElement('div');
+        criar_card.classList.add('card');
+        class_menuPizza.appendChild(criar_card);
+
+    let class_card = document.querySelectorAll('.card');
+    let criar_imgBox = document.createElement('div');
+        criar_imgBox.classList.add('imgBox');
+        class_card[i].appendChild(criar_imgBox);
+
+    let class_imgBox = document.querySelectorAll('.imgBox');
+    let criar_img = document.createElement('img');
+        criar_img.src = menuPizza[i].src;
+        class_imgBox[i].appendChild(criar_img);
+
+    let criar_nomePizza = document.createElement('div');
+        criar_nomePizza.classList.add('nomepizza');
+        class_imgBox[i].appendChild(criar_nomePizza);
+
+    let class_nomePizza = document.querySelectorAll('.nomepizza');
+    let criar_nomePizza_h1 = document.createElement('h1');
+        criar_nomePizza_h1.innerHTML = menuPizza[i].nome;
+        class_nomePizza[i].appendChild(criar_nomePizza_h1);
+
+    let criar_details = document.createElement('div');
+        criar_details.classList.add('details');
+        class_card[i].appendChild(criar_details);
+
+    let class_details =  document.querySelectorAll('.details');
+    let criar_content = document.createElement('div');
+        criar_content.classList.add('content');
+        class_details[i].appendChild(criar_content);
+
+    let class_content = document.querySelectorAll('.details .content');
+    let criar_content_h2 = document.createElement('h2');
+        criar_content_h2.innerHTML = `${menuPizza[i].nome} <br><span>${menuPizza[i].ingreditente}</span>`
+        class_content[i].appendChild(criar_content_h2);
+
+    let criar_content_p = document.createElement('p');
+        criar_content_p.innerHTML = `R$ ${menuPizza[i].preco}`;
+        class_content[i].appendChild(criar_content_p);
+}
